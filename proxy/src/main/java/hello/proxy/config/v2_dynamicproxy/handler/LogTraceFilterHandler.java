e42a4eb5d6ce1ca76686e7253971181827f90ccf
gombasan <xogmlwkdi@naver.com>
2023-09-04 23:15:19 +0900

    section 5 : jdk 동적 프록시
    - 프록시를 생성 후 테스트 확인
    - 동적 프록시를 실제 애플리케이션에 적용
    - filter 로 로그를 남기고 싶지 않은 경우 얼리리턴 활용

---
+++ proxy/src/main/java/hello/proxy/config/v2_dynamicproxy/handler/LogTraceFilterHandler.java
@@ -0,0 +1,43 @@
+package hello.proxy.config.v2_dynamicproxy.handler;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+import org.springframework.util.PatternMatchUtils;
+
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class LogTraceFilterHandler implements InvocationHandler {
+
+	private final Object target;
+
+	private final LogTrace trace;
+
+	private final String[] patterns;
+
+	@Override
+	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+		String methodName = method.getName();
+
+		if (!PatternMatchUtils.simpleMatch(patterns, methodName)) {
+			return method.invoke(target, args);
+		}
+
+		TraceStatus status = null;
+		try {
+			String message = method.getDeclaringClass().getSimpleName() + "." + method.getName() + "()";
+			status = trace.begin(message);
+			// 로직 호출
+			Object result = method.invoke(target, args);
+			trace.end(status);
+			return result;
+		}catch (Exception e){
+			trace.exception(status, e);
+			throw e;
+		}
+	}
+}
