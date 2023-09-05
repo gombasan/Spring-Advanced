e891f4f6189e7453be86724b147a419fc53059a5
gombasan <xogmlwkdi@naver.com>
2023-09-05 23:26:13 +0900

    section 6 : 프록시 팩토리 적용
    - 프록시 객체를 동적으로 생성하도록 하였어도, 인터페이스의 유무의 따라 프록시 객체가 구현해야할 인터페이스가 달라지는 불편함을 해소할수있다.
    - 하지만 아직 설정을 버전마다 계속해서 생성해야하는 불편함이 존재하며, 컴포넌트 스캔으로 생성된 빈에는 내가 프록시의 기능을 사용할 방법이 딱히 없는 문제가 있다.

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -5,17 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
-import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
-import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
-import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 
 // @Import(AppV1Config.class)
 // @Import({AppV1Config.class, AppV2Config.class})
 // @Import({InterfaceProxyConfig.class, ConcreteProxyConfig.class})
-@Import(DynamicProxyFilterConfig.class)
+// @Import(DynamicProxyFilterConfig.class)
+@Import({ProxyFactoryConfigV1.class, ProxyFactoryConfigV2.class})
 @SpringBootApplication(scanBasePackages = "hello.proxy.app.v3")
 public class ProxyApplication {
 
