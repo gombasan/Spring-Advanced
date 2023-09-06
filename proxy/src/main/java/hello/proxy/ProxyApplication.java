26b2fcf407c8d29c92871a047387789f13fd6cd4
gombasan <xogmlwkdi@naver.com>
2023-09-06 23:42:34 +0900

    section 7 : 스프링이 제공하는 `AutoProxyCreate`
    - 스프링 부트는 기본으로 등록하는 빈 객체가 많다.
    따라서 패키지명으로 프록시를 등록하게 된다면, 굳이 필요없는 객체 또한 프록시 객체를 생성할수도 있다.
    - 이 문제점을 해결하기 위해 스프링에서는 `AnnotationAwareAspectJAutoProxyCreator` 를 사용하여 자동으로 필요한 객체에만 프록시 객체를 생성한 후에 컨테이너에 등록도 해주게 된다.
    - 프록시 객체가 필요하다고 판단하게 되는 근거는 컨테이너에 등록된 `Advisor` 에 있는 `PointCut` 을 사용하여, 등록하는 객체가 `PointCut` 에 하나라도 해당되면 프록시 객체를 생성한다.
    - 생성된 프록시 객체는 `PointCut` 을 기준으로 메서드가 실행될 때 `Advice` 를 실행하게 된다.
    - 만약 등록된 `Advisor` 가 여러개라면 모든 `Advisor` 내에 `PointCut` 을 보고 객체를 등록 할 때 필요한 `Advisor` 를 모두 함께 프록시 객체와 같이 등록하게 된다.

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Import;
 
 import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
 import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
+import hello.proxy.config.v4_postprocessor.postprocessor.BeanPostProcessorConfig;
+import hello.proxy.config.v5_autoproxy.AutoProxyConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 
@@ -14,7 +16,9 @@ import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 // @Import({AppV1Config.class, AppV2Config.class})
 // @Import({InterfaceProxyConfig.class, ConcreteProxyConfig.class})
 // @Import(DynamicProxyFilterConfig.class)
-@Import({ProxyFactoryConfigV1.class, ProxyFactoryConfigV2.class})
+// @Import({ProxyFactoryConfigV1.class, ProxyFactoryConfigV2.class})
+// @Import(BeanPostProcessorConfig.class)
+@Import(AutoProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app.v3")
 public class ProxyApplication {
 
