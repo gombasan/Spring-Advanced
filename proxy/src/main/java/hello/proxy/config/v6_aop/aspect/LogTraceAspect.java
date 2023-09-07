a8ba8f932d8b840a766be999355c7036a587aa77
gombasan <xogmlwkdi@naver.com>
2023-09-07 22:56:17 +0900

    section 8 : 스프링 @Aspect 적용
    - 스프링에서 지원하는 @Aspect 어노테이션을 사용하여 `Advisor` 를 생성한다.
    - 스프링은 컨테이너 내부에서 @Aspect 어노테이션이 적용된 클래스를 조회 하여 `Advisor Builder` 에 담아 `Advisor` 를 만들어준다.
    - 이후 스프링이 빈을 등록 하려할때 후처리기는 스프링 컨테이너와  `Advisor Builder` 에서 `Advisor` 를 찾게 된다.
    - 프록시 객체를 생성하는 것은 기존과 동일
    - @Around 로 PointCut 과 Advice 를 표현하여 `Advisor` 를 만들어 낼 수 있다.
    - @Aspect 어노테이션이 붙은 클래스를 빈에 등록만 하게 되면, 위와 같은 행위를 실행시킬수있다.

---
+++ proxy/src/main/java/hello/proxy/config/v6_aop/aspect/LogTraceAspect.java
@@ -0,0 +1,37 @@
+package hello.proxy.config.v6_aop.aspect;
+
+import java.lang.reflect.Method;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Aspect
+public class LogTraceAspect {
+
+	private final LogTrace trace;
+
+	public LogTraceAspect(LogTrace trace) {
+		this.trace = trace;
+	}
+
+	@Around("execution(* hello.proxy.app..*(..))")
+	public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+		TraceStatus status = null;
+		try {
+			String message = joinPoint.getSignature().toShortString();
+			status = trace.begin(message);
+			Object result = joinPoint.proceed();
+			trace.end(status);
+			return result;
+		}catch (Exception e){
+			trace.exception(status, e);
+			throw e;
+		}
+	}
+}
